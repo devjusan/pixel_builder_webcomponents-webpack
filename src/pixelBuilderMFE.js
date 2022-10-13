821287630e7b6023a828419b504d5b4b6097c840
Valmir Santos <jnrplay@hotmail.com>
2022-10-13 14:09:31 -0300

    style: h5

--- src/pixelBuilderMFE.js
+++ src/pixelBuilderMFE.js
@@ -2,27 +2,21 @@ const app = {
   use(plugin, widgetInstance) {
     console.log("has been tryied to install plugin", plugin, widgetInstance);
   },
-  mount(rootElement) {
+  async mount(rootElement) {
     const wrapper = document.querySelector(rootElement);
     const pixelBuilderContainer = document.createElement("div");
     pixelBuilderContainer.setAttribute("id", "pixel_builder_container");
 
     wrapper.appendChild(pixelBuilderContainer);
 
-    import("./index.js");
-    // import("./pixel/insertCdn.js").then(async () => {
-    //   await import("./pixel/modules.js").then(async () => {
-    //     const container = document.getElementById("pixel_builder_container");
-    //     const stageEl = document.createElement("widget-stage");
-    //     stageEl.id = "pixelbuilder_valuation";
-    //     container.appendChild(stageEl);
-
-    //     await import("./pixel/scopedStyles.js");
-    //   });
-    // });
+    await import("./index.js");
   },
   unmount(...args) {
-    console.log("unmount", ...args);
+    import("./pixel/dependencies/services/modals.service.js").then(
+      (modalsService) => {
+        modalsService.default.close();
+      }
+    );
   },
   config: {
     globalProperties: {},
